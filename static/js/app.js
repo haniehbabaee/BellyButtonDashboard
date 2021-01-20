f060f8e3581107be8ce5d9904e6aef74282c4895
haniehbabaee <70447525+haniehbabaee@users.noreply.github.com>
2021-01-20 08:08:27 -0500

    update

---
+++ static/js/app.js
@@ -0,0 +1,77 @@
+function retrievedata(){
+    d3.json("samples.json").then(function(data){
+    console.log(data)
+
+})};
+retrievedata();
+
+
+// On change to the DOM, call BuildChart()
+d3.selectAll("#selDataset").on("change", BuildChart);
+
+
+function BuildChart(){
+    d3.json("samples.json").then(data=>{
+        
+        
+        //creating ID for the input of Test Subject ID No.
+        var ID = data.samples.map(object=>object.id) ;
+        //console.log(ID[0]);
+        var selection =d3.select("#selDataset")
+        for (var i = 0; i < 153; i++) {
+            selection.append("option").text(ID[i]).property("value(ID[i])")
+        }
+
+        //getting the dropdownMenuValue
+        var dropdownMenuValue = d3.select("#selDataset").node().value;
+        console.log(dropdownMenuValue.toString());
+
+        //getting x and y values
+        var filteredResult= data.samples.filter(itemObj=> itemObj.id == dropdownMenuValue);
+        console.log(filteredResult);
+        var values= filteredResult[0].sample_values;
+        console.log(values);
+        var ids= filteredResult[0].otu_ids;
+        console.log(ids);
+        var names=filteredResult[0].otu_labels.slice(0,10);
+        console.log(names);
+        
+        //creating horizontal bar chart
+        var trace1={
+            x: values.slice(0,10).reverse(),
+            y: ids.slice(0,10).map(otu_ids=>`OTU ${otu_ids}`).reverse(),
+            text: names,
+            type:"bar",
+            orientation: "h"
+        };
+        var data1=[trace1];
+        var layout1= {
+            margin:{t:50, l:175}
+        };
+        Plotly.newPlot("bar", data1, layout1)
+
+        //creating bubble chart
+        var trace2={
+            x: ids,
+            y: values,
+            mode:"markers",
+            marker:{
+                size:values,
+                color:ids
+            },
+            text: names
+        };
+        var data2=[trace2];
+        var layout2={
+            xaxis:{title:"OTU ID"},
+            height: 600,
+            width: 1200
+        }
+        Plotly.newPlot("bubble", data2, layout2)
+        
+        //creating demo graphics info
+        var filteredMeta=data.metadata.filter(itemObj=> itemObj.id == dropdownMenuValue);
+        console.log(filteredMeta)
+    })
+}
+BuildChart()
